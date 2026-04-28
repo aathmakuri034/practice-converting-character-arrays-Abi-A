195cdd0b01a8834662e0c57ed38c427f479c45bc
Abi Athmakuri <aathmakuri034@gmail.com>
2026-04-28 13:08:41 -0500

    assignment done

---
+++ main.js
@@ -0,0 +1,20 @@
+let message = " !yako eb ll\'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll\'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+console.log(message.split('').reverse().join(''));
+
+message = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+console.log(message.split('').reverse().join(''));
+
+message = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+console.log(message.split('').reverse().join(''));
+
+message = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+console.log(message.split('').reverse().join(''));
+
+message = "Everything works out in the end";
+console.log(message.split('').reverse().join(''));
+
+message = "Life isn't worth living, unless it is lived for someone else.";
+console.log(message.split('').reverse().join(''));
+
+message = "Everyone thinks of changing the world, but no one thinks of changing himself";
+console.log(message.split('').reverse().join(''));
